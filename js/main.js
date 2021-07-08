60f5cf412efab873396ffc218fd5397d20190ce7
Perseu <josimar.17perseu@gmail.com>
2021-07-07 21:26:05 -0300

    corrigindo erro que não estava fazeno a cobra movimentar, jogo completo agora

--- js/main.js
+++ js/main.js
@@ -23,34 +23,35 @@ function criarBG(){
 
 }
 
-var cabeca = new Image();
-cabeca.src = 'img/cabeca.png';
+
+
 
 function criarCobra(){
-
-    cabeca = context.createPattern(cabeca, 'repeat');
-    context.fillStyle = cabeca;
-    context.fillRect(cobra[0].x, cobra[0].y , box, box);
-
-    for(i = 1 ; i < cobra.length ; i++){
+    var cabeca = new Image();
+    cabeca.src = 'img/cabeca.png';
+    var cabeca_cobra = context.createPattern(cabeca, 'repeat');
+    for(i = 0 ; i < cobra.length ; i++){
         context.fillStyle = "green";
+        if(i == 0){
+            context.fillStyle = cabeca_cobra;
+        }
         context.fillRect(cobra[i].x, cobra[i].y , box, box);
     }
 }
 
-var img = new Image();
-img.src = 'img/maca.png';
 
 function drawfood(){
-    fruta = context.createPattern(img, 'repeat');
+    var img = new Image();
+    img.src = 'img/maca.png';
+    var fruta = context.createPattern(img, 'repeat');
     context.fillStyle = fruta;
     context.fillRect(food.x, food.y, box , box)
 
 }
 
+document.addEventListener('keydown', update);
 
 function update (event){
-
     
     if(event.keyCode == 37 && direcao != "right"){
         direcao = "left";
@@ -70,28 +71,29 @@ function update (event){
 
 function iniciarJogo(){
 
+    
     if(cobra[0].x > 15 * box && direcao == "right"){
         cobra[0].x = 0;
     }
     if(cobra[0].x < 0 && direcao == "left"){
-        cobra[0].x = 16 * box;
+        cobra[0].x = 15 * box;
     }
     if(cobra[0].y > 15 * box && direcao == "down"){
         cobra[0].y = 0;
     }
     if(cobra[0].y < 0 && direcao == "up"){
-        cobra [0].y = 16 * box;
+        cobra [0].y = 15 * box;
     }
 
-    criarBG();
-    criarCobra(); 
+    criarBG(); 
     drawfood();
+    criarCobra();
  
 
     let cobraX = cobra[0].x;
     let cobraY = cobra[0].y;
 
-    if(direcao =="right"){
+    if(direcao == "right"){
         cobraX += box;
     }
     if(direcao =="left"){
@@ -113,12 +115,11 @@ function iniciarJogo(){
     }
 
     let newhead = {
-        x:cobrax,
-        Y:cobray
+        x: cobraX,
+        y: cobraY
     }
-    cobra.unshift(newhead);
-    
+
+    cobra.unshift(newhead);     
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
